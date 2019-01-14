478a17beea5da05fd5641d1d2f2adfc6292e0184
Jeremias <jeremias1098@gmail.com>
2019-01-14 20:39:23 +0100

    Save anfang

--- lib/backend/Saves.dart
+++ lib/backend/Saves.dart
@@ -2,13 +2,19 @@ import 'package:school_organizer/backend/hausaufgaben/Hausaufgabe.dart';
 import 'package:school_organizer/backend/pruefungen/Pruefung.dart';
 import 'package:school_organizer/backend/stundenplan/Fach.dart';
 import 'package:school_organizer/backend/stundenplan/Lehrer.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 class Saves {
-
-  String
+  String hausaufgaben_key = "hausaufgaben_key234423";
+  String faecher_key = "faecher_key2445623";
+  String lehrer_key = "lehrer_key1234321";
+  String pruefungen_key = "pruefungen_key1234532";
 
   List<Hausaufgabe> hausaufgaben;
   List<Fach> faecher;
   List<Lehrer> lehrer;
   List<Pruefung> pruefungen;
+
+  SharedPreferences prefs;
 }
--- lib/backend/stundenplan/Lehrer.dart
+++ lib/backend/stundenplan/Lehrer.dart
@@ -1,6 +1,17 @@
 class Lehrer {
-  String name;
-  String geschlecht;
+  String _name;
+  String _geschlecht;
 
-  Lehrer(this.name, this.geschlecht);
+  Lehrer(this._name, this._geschlecht);
+
+  Lehrer.fromJson(Map<String, dynamic> m) {
+    _name = m["name"];
+    _geschlecht = m["geschlecht"];
+  }
+
+  String get name => _name;
+
+  String get geschlecht => _geschlecht;
+
+  Map<String, dynamic> toJson() => {"name": _name, "geschlecht": _geschlecht};
 }
