aeb86422fe4a830209d70e6a7b59dc8d3218114e
Jeremias <jeremias1098@gmail.com>
2019-01-14 18:31:58 +0100

    dialog eingefügt

--- lib/home/AddEntryDialog.dart
+++ lib/home/AddEntryDialog.dart
@@ -6,6 +6,9 @@ class AddEntryDialog extends StatefulWidget {
 }
 
 class _AddEntryDialogState extends State<AddEntryDialog> {
+  int _radioValue = 0;
+  bool homework = true;
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -16,11 +19,77 @@ class _AddEntryDialogState extends State<AddEntryDialog> {
             onPressed: () {
               Navigator.of(context).pop("Kleiner Test");
             },
-            child: Text("Speichern", style: TextStyle(color: Colors.white),),
+            child: Text(
+              "Speichern",
+              style: TextStyle(color: Colors.white),
+            ),
           )
         ],
       ),
-      body: Text("Body"),
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                RadioListTile(
+                  groupValue: _radioValue,
+                  onChanged: _radioValueChange,
+                  value: 0,
+                  title: Text("Hausaufgabe"),
+                ),
+                RadioListTile(
+                  groupValue: _radioValue,
+                  onChanged: _radioValueChange,
+                  value: 1,
+                  title: Text("Prüfung"),
+                ),
+              ],
+            ),
+            Container(
+              child: Divider(),
+              padding: EdgeInsets.symmetric(horizontal: 40.0),
+            ),
+            homework ? Homework() : Test()
+          ],
+        ),
+      ),
     );
+  }
+
+  _radioValueChange(int value) {
+    setState(() {
+      _radioValue = value;
+      if (_radioValue == 0) {
+        homework = true;
+      } else {
+        homework = false;
+      }
+    });
+  }
+}
+
+class Homework extends StatefulWidget {
+  _HomeworkState createState() => _HomeworkState();
+}
+
+class _HomeworkState extends State<Homework> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hausaufgabe");
+  }
+}
+
+class Test extends StatefulWidget {
+  _TestState createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Prüfung");
   }
 }
