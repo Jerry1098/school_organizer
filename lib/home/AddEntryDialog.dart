fd32d04ce12d249ace50098191d3a7c6b11bb1a1
Jeremias <jeremias1098@gmail.com>
2019-01-16 09:16:01 +0100

    AddEntryCards color correction

--- lib/home/AddEntryDialog.dart
+++ lib/home/AddEntryDialog.dart
@@ -11,7 +11,7 @@ class _AddEntryDialogState extends State<AddEntryDialog> {
   Text title = Text("Neuer Eintrag");
   PageController controller = PageController();
   var currentPageValue = 0.0;
-  Color colorTest = Colors.orangeAccent.withOpacity(0.0);
+  Color colorTest = Colors.white;
   Color colorHomework = Colors.orangeAccent;
 
   @override
@@ -19,8 +19,11 @@ class _AddEntryDialogState extends State<AddEntryDialog> {
     controller.addListener(() {
       setState(() {
         currentPageValue = controller.page;
-        colorHomework = Colors.orangeAccent.withOpacity(1.0 - currentPageValue);
-        colorTest = Colors.orangeAccent.withOpacity(currentPageValue);
+        colorHomework = alphaBlend(
+            Colors.orangeAccent.withOpacity(1.0 - currentPageValue),
+            Colors.white);
+        colorTest = alphaBlend(
+            Colors.orangeAccent.withOpacity(currentPageValue), Colors.white);
       });
     });
     return Scaffold(
@@ -100,6 +103,36 @@ class _AddEntryDialogState extends State<AddEntryDialog> {
             ],
           ),
         ));
+  }
+
+  static Color alphaBlend(Color foreground, Color background) {
+    final int alpha = foreground.alpha;
+    if (alpha == 0x00) {
+      // Foreground completely transparent.
+      return background;
+    }
+    final int invAlpha = 0xff - alpha;
+    int backAlpha = background.alpha;
+    if (backAlpha == 0xff) {
+      // Opaque background case
+      return new Color.fromARGB(
+        0xff,
+        (alpha * foreground.red + invAlpha * background.red) ~/ 0xff,
+        (alpha * foreground.green + invAlpha * background.green) ~/ 0xff,
+        (alpha * foreground.blue + invAlpha * background.blue) ~/ 0xff,
+      );
+    } else {
+      // General case
+      backAlpha = (backAlpha * invAlpha) ~/ 0xff;
+      final int outAlpha = alpha + backAlpha;
+      assert(outAlpha != 0x00);
+      return new Color.fromARGB(
+        outAlpha,
+        (foreground.red * alpha + background.red * backAlpha) ~/ outAlpha,
+        (foreground.green * alpha + background.green * backAlpha) ~/ outAlpha,
+        (foreground.blue * alpha + background.blue * backAlpha) ~/ outAlpha,
+      );
+    }
   }
 
   _radioValueChange(int value) {
